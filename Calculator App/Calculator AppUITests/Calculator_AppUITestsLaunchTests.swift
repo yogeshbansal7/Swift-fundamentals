f824f02fadf2fdbb5315c660cb83b8a0c5372e6e
Yogesh Bansal <101383618+yogeshbansal7@users.noreply.github.com>
2024-04-15 09:45:01 +0530

    Add files via upload

---
+++ Calculator App/Calculator AppUITests/Calculator_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Calculator_AppUITestsLaunchTests.swift
+//  Calculator AppUITests
+//
+//  Created by student on 23/02/24.
+//
+
+import XCTest
+
+final class Calculator_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
